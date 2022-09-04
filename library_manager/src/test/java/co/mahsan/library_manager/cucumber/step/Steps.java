8e1e51968a61c1a6d3de377ba9bea996fc719a6e
m.baghban <m.baghban@mahsan.co>
2022-09-04 10:31:03 +0430

    Added new test scenarios.

--- library_manager/src/test/java/co/mahsan/library_manager/cucumber/step/Steps.java
+++ library_manager/src/test/java/co/mahsan/library_manager/cucumber/step/Steps.java
@@ -2,27 +2,39 @@ package co.mahsan.library_manager.cucumber.step;
 
 import co.mahsan.library_manager.controller.BookController;
 import co.mahsan.library_manager.model.BookDTO;
+import co.mahsan.library_manager.repository.BookRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class Steps {
 
     private final BookController bookController;
+    private final BookRepository bookRepository;
+    private String lastBookName = "";
+    private String lastBookId = "";
+    private HttpStatus lastStatusCode;
+    private final String baseUrl = "http://localhost:8080";
     //this method executes after every scenario
     @After
     public void cleanUp() {
@@ -40,7 +52,7 @@ public class Steps {
     @Before
     public void before() {
         log.info(">>> Before scenario!");
-        //placeholder for before scenario logic
+        lastBookName = new Date().toString();
     }
 
     //this method executes before every step
@@ -50,26 +62,68 @@ public class Steps {
         //placeholder for before step logic
     }
 
-    @When("the client calls book {string}")
-    public void theClientCallsBook(String arg0) throws JsonProcessingException {
+    @Given("execute posting a new book")
+    public void postNewBook() throws IOException {
+        BookDTO newBookDTO = new BookDTO();
+        newBookDTO.setName(lastBookName);
+        ObjectMapper mapper = new ObjectMapper();
 
-        ResponseEntity<String> response = executeGet("http://localhost:8080/books/" + arg0);
+        String jsonInString = mapper.writeValueAsString(newBookDTO);
+        System.out.println("new bookdto " + jsonInString);
+        ResponseEntity<String> response = executePost(baseUrl + "/books/", jsonInString);
+        lastBookId = mapper.readValue(response.getBody(), BookDTO.class).getId();
+
+    }
+
+    @Then("check the status code to be OK")
+    public void checkStatusCodeToBeOK() {
+
+        assertEquals(lastStatusCode, HttpStatus.OK);
+    }
+
+
+    @When("the client calls book by id")
+    public void theClientCallsBookById() throws JsonProcessingException {
+
+        ResponseEntity<String> response = executeGet(baseUrl + "/books/" + lastBookId);
         ObjectMapper objectMapper = new ObjectMapper();
         BookDTO bookDTO = objectMapper.readValue(response.getBody(), BookDTO.class);
         System.out.println("book received " + bookDTO.getName());
-        assertEquals(bookDTO.getName(), "new book");
+        assertEquals(bookDTO.getName(), lastBookName);
     }
 
 
     @When("the client calls books")
-    public void theClientCallsBooks() {
-        System.out.println("In call books");
-        assertTrue(false);
+    public void theClientCallsBooks() throws JsonProcessingException {
+        ResponseEntity<String> response = executeGet(baseUrl + "/books/");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<BookDTO> booksDTO = objectMapper.readValue(response.getBody(), new TypeReference<List<BookDTO>>(){});
+        System.out.println("books received " + booksDTO.get(0));
+        //assertEquals(bookDTO.getName(), "new book");
 
+    }
+
+    @Then("check if new book is in database")
+    public void checkIfNewBookIsInDB() {
+        assertFalse(bookRepository.findByName(lastBookName).isEmpty());
     }
 
     public ResponseEntity<String> executeGet(String url) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForEntity(url, String.class);
+        ResponseEntity<String> response =  restTemplate.getForEntity(url, String.class);
+        lastStatusCode = response.getStatusCode();
+        return response;
+    }
+
+    public ResponseEntity<String> executePost(String url, String requestJson) throws IOException {
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, entity ,String.class);
+        lastStatusCode = response.getStatusCode();
+        return response;
     }
 }
--- library_manager/src/test/resources/co/mahsan/library_manager/feature/scenarios.feature
+++ library_manager/src/test/resources/co/mahsan/library_manager/feature/scenarios.feature
@@ -1,6 +1,16 @@
 Feature: Books can be retrieved
   Scenario: client makes call to GET /books
     When the client calls books
+    Then check the status code to be OK
 
   Scenario: client makes call to GET /books/id
-    When the client calls book "630f2dd1f3642a6671c9cfcf"
+    Given execute posting a new book
+    When the client calls book by id
+    Then check the status code to be OK
+
+  Scenario: client makes post request
+    When execute posting a new book
+    Then check if new book is in database
+    And check the status code to be OK
+
+
